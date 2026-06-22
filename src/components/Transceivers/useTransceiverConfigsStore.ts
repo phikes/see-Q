663b70f371ac4ac060674a82041536cee2e81bfc
Phillip Kessels <info@phikes.de>
2026-06-22 16:25:24 +0200

    feat: persist transceiver configs

--- src/components/Transceivers/useTransceiverConfigsStore.ts
+++ src/components/Transceivers/useTransceiverConfigsStore.ts
@@ -1,16 +1,22 @@
 import { create } from "zustand"
 import type { TransceiverConfig } from "../TransceiverModal"
+import { persist } from "zustand/middleware"
 
 interface TransceiverConfigsState {
   addTransceiverConfig: (transceiverConfig: TransceiverConfig) => void
   transceiverConfigs: TransceiverConfig[]
 }
 
-export const useTransceiverConfigsStore = create<TransceiverConfigsState>((set) => ({
-  transceiverConfigs: [],
-  addTransceiverConfig: (transceiverConfig: TransceiverConfig) =>
-  set(({ transceiverConfigs, ...state }) => ({
-    ...state,
-    transceiverConfigs: [...transceiverConfigs, transceiverConfig]
-  }))
-}))
+export const useTransceiverConfigsStore = create<TransceiverConfigsState>()(
+  persist(
+    (set) => ({
+      transceiverConfigs: [],
+      addTransceiverConfig: (transceiverConfig: TransceiverConfig) =>
+      set(({ transceiverConfigs, ...state }) => ({
+        ...state,
+        transceiverConfigs: [...transceiverConfigs, transceiverConfig]
+      }))
+    }),
+    {name: "transceiver-config-store"}
+  )
+)
